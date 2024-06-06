fd2d1f138a327792ecd5e06e8a1a23e375a9536c
Võ Phước Thạnh <92651849+vophuocthanh@users.noreply.github.com>
2024-06-06 12:05:21 +0700

    feat(FE): connect db mongoose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import { connectToDattabase } from '@/lib/mongoose';
+
+export default async function Home() {
+  const connect = connectToDattabase();
+  console.log('connect:', connect);
   return <div>Hompage</div>;
 }
---
+++ src/lib/mongoose.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import mongoose from 'mongoose';
+
+// signleton connection: có nghĩa là nếu kết nối rồi thì không kết nối nữa, còn nếu chưa kết nối thì nó sẽ kết nối.
+
+let isConnected: boolean = false;
+
+export const connectToDattabase = async () => {
+  if (!process.env.MONGODB_URL) {
+    throw new Error('MONGODB_URL is not defined');
+  }
+
+  if (isConnected) {
+    console.log('Already connected to database');
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: 'Ucademy',
+    });
+    isConnected = true;
+    console.log('Using new database connection');
+  } catch (error) {
+    console.log('Error while connecting to database');
+  }
+};
