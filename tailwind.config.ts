478a8559ca388d18d90b789686519c4de024852a
vophuocthanh <phuocthanh2k03@gmail.com>
2024-06-18 14:22:28 +0700

    feat(FE): update coure new v2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: '#ff979a',
+        primary: '#4E06FE',
         grayDarkest: '#131316',
         grayDarker: '#212126',
         grayDark: '#9394A1',
