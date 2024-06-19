5ee046f9c81e668c37b7def2a64e11fc4a74cd52
vophuocthanh <phuocthanh2k03@gmail.com>
2024-06-19 09:14:33 +0700

    feat(FE): add cource update

--- src/app/(dashboard)/manage/course/new/page.tsx
+++ src/app/(dashboard)/manage/course/new/page.tsx
@@ -1,12 +1,19 @@
 import Heading from '@/components/common/Heading';
 import CourseAddNew from '@/components/cource/CourceAddNew';
+import { getUserInfo } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
 
-const page = () => {
+const page = async () => {
+  const { userId } = auth();
+  if (!userId) return null;
+  const mongoUser = await getUserInfo({ userId });
+  if (!mongoUser) return null;
   return (
     <>
       <Heading>Tạo khóa học mới</Heading>
-      <CourseAddNew></CourseAddNew>
+      <CourseAddNew user={JSON.parse(JSON.stringify(mongoUser))}></CourseAddNew>
     </>
   );
 };
+
 export default page;
