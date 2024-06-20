6bad5583570a8ac24589af40c38f068b746bb58d
vophuocthanh <phuocthanh2k03@gmail.com>
2024-06-20 16:48:42 +0700

    update code

--- src/app/(dashboard)/study/page.tsx
+++ src/app/(dashboard)/study/page.tsx
@@ -1,17 +1,15 @@
-import { CourceGrid } from '@/components/common';
-import CourseItem from '@/components/cource/CourceItem';
 import Heading from '@/components/typography/Heading';
 
 export default function page() {
   return (
     <>
       <Heading>Khu vực học tập</Heading>
-      <CourceGrid>
-        <CourseItem image='https://soshace.com/wp-content/uploads/2019/10/Getting-Started-with-NextJS-Inside.jpg' />
+      {/* <CourceGrid>
+        <CourseItem  image='https://soshace.com/wp-content/uploads/2019/10/Getting-Started-with-NextJS-Inside.jpg' />
         <CourseItem image='https://www.perfomatix.com/wp-content/uploads/2023/02/Next.js-13-All-you-need-to-know.png' />
         <CourseItem image='https://kb.pavietnam.vn/wp-content/uploads/2022/10/nextjs-1.png' />
         <CourseItem image='https://www.syncfusion.com/blogs/wp-content/uploads/2022/09/Optimize-NextJS-App-bundle-performance.png' />
-      </CourceGrid>
+      </CourceGrid> */}
     </>
   );
 }
