8a1b0cc7b907c16d8b9090f0df735ea2d3e2573c
vophuocthanh <phuocthanh2k03@gmail.com>
2024-06-24 22:19:09 +0700

    feat(FE): add UI landing page course details 🚀🚀

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='wrapper grid grid-cols-[300px,minmax(0,1fr)] h-screen'>
+    <div className='wrapper grid lg:grid-cols-[300px,minmax(0,1fr)] h-screen'>
       <Sidebar />
       <main className='p-5 overflow-y-auto'>{children}</main>
     </div>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -10,7 +10,7 @@ import { ActiveLink } from '../common';
 const Sidebar = () => {
   const { userId } = useAuth();
   return (
-    <div className='p-5 border-r overflow-hidden border-r-gray-200 dark:border-opacity-10 bg-white dark:bg-grayDarker flex flex-col'>
+    <div className='hidden p-5 border-r border-r-gray-200 dark:border-opacity-10 bg-white dark:bg-grayDarker lg:flex flex-col'>
       <a href='/' className='font-bold text-3xl inline-block mb-5'>
         <span className='text-primary'>U</span>
         cademy
