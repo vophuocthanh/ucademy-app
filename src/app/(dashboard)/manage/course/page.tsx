318607e1f90b683ea1ea48fb5b25bdfa3c1422c1
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-11-01 21:18:06 +0700

    feat: add functional filter, pagination course

--- src/app/(dashboard)/manage/course/page.tsx
+++ src/app/(dashboard)/manage/course/page.tsx
@@ -1,13 +1,23 @@
 import CourseManage from '@/components/cource/CourceManager';
 import { getAllCourses } from '@/lib/actions/cource.actions';
+import { ECourseStatus } from '@/types/enums';
 
-const page = async () => {
-  const courses = await getAllCourses();
-  return (
-    <CourseManage
-      courses={courses ? JSON.parse(JSON.stringify(courses)) : []}
-    ></CourseManage>
-  );
+const page = async ({
+  searchParams,
+}: {
+  searchParams: {
+    page: number;
+    search: string;
+    status: ECourseStatus;
+  };
+}) => {
+  const courses = await getAllCourses({
+    page: searchParams.page || 1,
+    limit: 10,
+    search: searchParams.search || '',
+    status: searchParams.status || ECourseStatus.APPROVED,
+  });
+  return <CourseManage courses={courses ? JSON.parse(JSON.stringify(courses)) : []}></CourseManage>;
 };
 
 export default page;
--- src/components/cource/CourceManager.tsx
+++ src/components/cource/CourceManager.tsx
@@ -1,5 +1,13 @@
 'use client';
 import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import {
   Table,
   TableBody,
   TableCell,
@@ -12,47 +20,35 @@ import { ICourse } from '@/database/cource.model';
 import { updateCourse } from '@/lib/actions/cource.actions';
 import { cn } from '@/lib/utils';
 import { ECourseStatus } from '@/types/enums';
+import { debounce } from 'lodash';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import Swal from 'sweetalert2';
 import Heading from '../common/Heading';
-import { IconDelete, IconEdit, IconEye, IconStudy } from '../icons';
+import { IconDelete, IconEdit, IconEye, IconLeftArrow, IconRightArrow, IconStudy } from '../icons';
 import { Input } from '../ui/input';
 
-const IconArrowLeft = (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    strokeWidth={1.5}
-    stroke='currentColor'
-    className='w-6 h-6'
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18'
-    />
-  </svg>
-);
-const IconArrowRight = (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    strokeWidth={1.5}
-    stroke='currentColor'
-    className='w-6 h-6'
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
-    />
-  </svg>
-);
 const CourseManage = ({ courses }: { courses: ICourse[] }) => {
+  const [page, setPage] = useState(1);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
   const handleDeleteCourse = (slug: string) => {
     Swal.fire({
       title: 'Are you sure?',
@@ -76,6 +72,7 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
       }
     });
   };
+
   const handleChangeStatus = async (slug: string, status: ECourseStatus) => {
     try {
       Swal.fire({
@@ -91,9 +88,7 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
           await updateCourse({
             slug,
             updateData: {
-              status: ECourseStatus.PENDING
-                ? ECourseStatus.APPROVED
-                : ECourseStatus.PENDING,
+              status: ECourseStatus.PENDING ? ECourseStatus.APPROVED : ECourseStatus.PENDING,
               _destroy: false,
             },
             path: '/manage/course',
@@ -105,6 +100,26 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
       console.log(error);
     }
   };
+
+  const handleSelectStatus = (status: ECourseStatus) => {
+    router.push(`${pathname}?${createQueryString('status', status)}`);
+  };
+
+  const handleSearchCourse = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    router.push(`${pathname}?${createQueryString('search', e.target.value)}`);
+  }, 500);
+
+  const handleChangePage = (type: 'prev' | 'next') => {
+    if (type === 'prev' && page === 1) return;
+    if (type === 'prev') setPage((prev) => prev - 1);
+    if (type === 'next') setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    router.push(`${pathname}?${createQueryString('page', page.toString())}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
   return (
     <>
       <Link
@@ -119,17 +134,29 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
           stroke='currentColor'
           className='w-6 h-6'
         >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M12 4.5v15m7.5-7.5h-15'
-          />
+          <path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
         </svg>
       </Link>
       <div className='flex flex-col lg:flex-row lg:items-center gap-5 justify-between mb-10'>
         <Heading className=''>Quản lý khóa học</Heading>
-        <div className='w-full lg:w-[300px]'>
-          <Input placeholder='Tìm kiếm khóa học...' />
+        <div className='flex gap-3'>
+          <div className='w-full lg:w-[300px]'>
+            <Input placeholder='Tìm kiếm khóa học...' onChange={(e) => handleSearchCourse(e)} />
+          </div>
+          <Select onValueChange={(value) => handleSelectStatus(value as ECourseStatus)}>
+            <SelectTrigger className='w-[180px]'>
+              <SelectValue placeholder='Chọn trạng thái' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {courseStatus.map((status) => (
+                  <SelectItem value={status.value} key={status.value}>
+                    {status.title}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
       </div>
       <Table className='table-responsive'>
@@ -144,9 +171,7 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
         <TableBody>
           {courses.length > 0 &&
             courses.map((course) => {
-              const courseStatusItem = courseStatus.find(
-                (item) => item.value === course.status
-              );
+              const courseStatusItem = courseStatus.find((item) => item.value === course.status);
               return (
                 <TableRow key={course.slug}>
                   <TableCell>
@@ -163,9 +188,7 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
                           {course.title}
                         </h3>
                         <h4 className='text-xs lg:text-sm text-slate-500'>
-                          {new Date(course.created_at).toLocaleDateString(
-                            'vi-VI'
-                          )}
+                          {new Date(course.created_at).toLocaleDateString('vi-VI')}
                         </h4>
                       </div>
                     </div>
@@ -178,13 +201,8 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
                   <TableCell>
                     <button
                       type='button'
-                      className={cn(
-                        commonClassNames.status,
-                        courseStatusItem?.className
-                      )}
-                      onClick={() =>
-                        handleChangeStatus(course.slug, course.status)
-                      }
+                      className={cn(commonClassNames.status, courseStatusItem?.className)}
+                      onClick={() => handleChangeStatus(course.slug, course.status)}
                     >
                       {courseStatusItem?.title}
                     </button>
@@ -224,11 +242,17 @@ const CourseManage = ({ courses }: { courses: ICourse[] }) => {
         </TableBody>
       </Table>
       <div className='flex justify-end gap-3 mt-5'>
-        <button className={commonClassNames.paginationButton}>
-          {IconArrowLeft}
+        <button
+          className={commonClassNames.paginationButton}
+          onClick={() => handleChangePage('prev')}
+        >
+          <IconLeftArrow />
         </button>
-        <button className={commonClassNames.paginationButton}>
-          {IconArrowRight}
+        <button
+          className={commonClassNames.paginationButton}
+          onClick={() => handleChangePage('next')}
+        >
+          <IconRightArrow />
         </button>
       </div>
     </>
